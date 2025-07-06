4af65718ff0d38ce8dcdd02bde4c98f43166032b
Saklain <aldaiansaklain@gmail.com>
2025-07-06 14:34:32 +0600

    cholo shobai

--- src/main/java/com/example/owlpost_2_0/Client/ChatClient.java
+++ src/main/java/com/example/owlpost_2_0/Client/ChatClient.java
@@ -1,6 +1,5 @@
 package com.example.owlpost_2_0.Client;
 
-
 import com.example.owlpost_2_0.ChatRoom.ChatMessage;
 import javafx.application.Platform;
 
@@ -14,9 +13,11 @@ public class ChatClient {
     private ObjectOutputStream out;
     private ObjectInputStream in;
     private boolean isConnected = false;
+    private String username;
 
     // connects to the server in the chatroom using the ip address and username
     public ChatClient(String serverIP, String username) throws Exception{
+        this.username = username;
         socket = new Socket(serverIP, 1234);
         out = new ObjectOutputStream(socket.getOutputStream());
         in = new ObjectInputStream(socket.getInputStream());
@@ -28,27 +29,66 @@ public class ChatClient {
     }
 
     public void sendMessage(ChatMessage msg) throws Exception{
-        out.writeObject(msg);
-        out.flush();
+        if (isConnected) {
+            out.writeObject(msg);
+            out.flush();
+        }
     }
 
     // continuously listens for message in a different thread
     public void listenForMsg(Consumer<ChatMessage> messageHandler) {
         new Thread(() -> {
             try {
-                while (true) {
+                while (isConnected) {
                     try {
                         ChatMessage msg = (ChatMessage) in.readObject();
                         Platform.runLater(() -> messageHandler.accept(msg));
-                    }catch (Exception e) {
+                    } catch (Exception e) {
+                        if (isConnected) {
+                            System.out.println("Error reading message: " + e.getMessage());
+                        }
                         break;
                     }
                 }
-            }catch (Exception e) {
+            } catch (Exception e) {
                 System.out.println("Disconnected from server");
                 disconnect();
             }
         }).start();
+    }
+
+    // Send call initiation signal
+    public void initiateCall(String receiverUsername, String callType) throws Exception {
+        if (isConnected) {
+            ChatMessage callMsg = new ChatMessage(username, receiverUsername, callType);
+            sendMessage(callMsg);
+        }
+    }
+
+    // Send call response signal
+    public void respondToCall(String callerUsername, String response) throws Exception {
+        if (isConnected) {
+            ChatMessage responseMsg = new ChatMessage(username, callerUsername, response);
+            sendMessage(responseMsg);
+        }
+    }
+
+    // Send call end signal
+    public void endCall(String otherUsername) throws Exception {
+        if (isConnected) {
+            ChatMessage endMsg = new ChatMessage(username, otherUsername, "CALL_ENDED");
+            sendMessage(endMsg);
+        }
+    }
+
+    // Get connection status
+    public boolean isConnected() {
+        return isConnected && socket != null && !socket.isClosed();
+    }
+
+    // Get username
+    public String getUsername() {
+        return username;
     }
 
     public void disconnect() {
